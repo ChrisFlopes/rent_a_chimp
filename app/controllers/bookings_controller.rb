a6a9f54126080c22aa4a862049845c532a641013
Alexander Seyss <a@seyss.at>
2019-02-27 11:03:44 +0000

    pundit policies changed for booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,9 +6,21 @@ class BookingsController < ApplicationController
   end
 
   def new
+    @booking = Booking.new
+
+    authorize @booking
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    authorize @booking
+
+    @booking.animal = @animal
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +30,11 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:review).permit(:animal_id, :user_id, :start_date, :end_date)
   end
 end
