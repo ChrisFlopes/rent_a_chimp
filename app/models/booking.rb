1d19e6275ee930026d26dfc99b5d522bead6c160
Alexander Seyss <a@seyss.at>
2019-02-27 14:03:16 +0000

    validation dates for booking§

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,7 +7,7 @@ class Booking < ApplicationRecord
   validate :end_date_after_start_date?
 
   def end_date_after_start_date?
-    if end_date < start_date
+    if end_date > start_date
       errors.add :end_date, "must be after start date"
     end
   end
