30bf9185e1beaccdc5af228b02917571f00e729a
Kevin Willson <willson.kevin@gmail.com>
2019-03-01 11:25:57 +0000

    added dependencies to Booking, length validation to Review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :booking
 
-  validates :content, presence: true
+  validates :content, presence: true, length: { minimum: 20 }
   validates :rating, presence: true, numericality: { only_integer: true }
   validates :rating, inclusion: { in: [1, 2, 3, 4, 5],
                                   message: "Rating must be between 1 and 5" }
